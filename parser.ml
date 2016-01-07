7dc23d92f833043e63f58e3676019b497049306b
jack <dongtianzheng@gmail.com>
2016-01-07 16:38:31 -0500

    finish parser and add test cases

--- parser.ml
+++ parser.ml
@@ -28,11 +28,30 @@ let token_to_string tok =
   | End -> "END"
 
 let rec regex_to_string r : string = 
+  let addParens str = 
+    "(" ^ str ^ ")"
+  in
   match r with
-  | Closure re -> (regex_to_string re) ^ "*"
+  | Closure re -> 
+      let res = (regex_to_string re) in 
+      if String.length res > 1 then (addParens res) ^ "*" else res ^ "*"
   | Char c -> Char.escaped c
-  | Concatenation (r1, r2) -> (regex_to_string r1) ^ (regex_to_string r2)
-  | Alternation (r1, r2) -> (regex_to_string r1) ^ "|" ^ (regex_to_string r2)
+  | Concatenation (r1, r2) -> 
+     (let res1 = regex_to_string r1 in
+      let res2 = regex_to_string r2 in
+      match ((String.length res1) > 1, (String.length res2) > 1) with
+      | (true, true) -> (addParens res1) ^ (addParens res2)
+      | (true, false) -> (addParens res1) ^ res2
+      | (false, true) -> res1 ^ (addParens res2)
+      | (false, false) -> res1 ^ res2)
+  | Alternation (r1, r2) -> 
+     (let res1 = regex_to_string r1 in
+      let res2 = regex_to_string r2 in
+      match ((String.length res1) > 1, (String.length res2) > 1) with
+      | (true, true) -> (addParens res1) ^ "|" ^ (addParens res2)
+      | (true, false) -> (addParens res1) ^ "|" ^ res2
+      | (false, true) -> res1 ^ "|" ^ (addParens res2)
+      | (false, false) -> res1 ^ "|" ^ res2)
   (*| Epsilon -> ""*)
 
 let string_to_char_list (str:string) : char list = 
@@ -99,13 +118,9 @@ and parse_T (l : token list) : (regex * token list) =
   let (a1, l1) = parse_F l in
   let (t, rest) = lookahead l1 in 
   match t with
-  | Alphabet c -> (Char c, rest)
-  | LParen -> 
-     (let (a, l1) = parse_S rest in
-      let (t1, l2) = lookahead l1 in
-      match t1 with
-      | RParen -> (a, l2)
-      | _ -> raise (IllegalExpression "Unbalanced parentheses"))
+  | Alphabet _ | LParen -> 
+      let (a2, l2) = parse_S l1 in
+      (Concatenation (a1, a2), l2)
   | _ -> (a1, l1)
 
 
@@ -139,7 +154,7 @@ let parse str : regex =
   let (a, t) = parse_S tok_list in
   List.iter (fun c -> print_string (" " ^ (token_to_string c))) t;
   print_endline "";
-  if t <> [End] then raise (IllegalExpression "last token is not END") else 
+  print_endline (regex_to_string a);
   a
 
 
--- parser_test.ml
+++ parser_test.ml
@@ -1,5 +1,12 @@
 open Parser
 
 
+
 let _ = 
-  Parser.parse "a(b|c)*"
+  assert(Parser.parse "a(b|c)*" = 
+        (Concatenation (Char 'a', (Closure (Alternation (Char 'b', Char 'c'))))));
+  assert(Parser.parse "a" = Char 'a');
+  assert(Parser.parse "a|b" = Alternation (Char 'a', Char 'b'));
+  assert(Parser.parse "aab" = Concatenation (Char 'a', Concatenation (Char 'a', Char 'b')));
+  assert(Parser.parse "a*(a|b)" = Concatenation (Closure (Char 'a'),   Alternation (Char 'a', Char 'b')))
+
