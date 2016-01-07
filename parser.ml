a5c57c18676de595b372aaa011d18655720b05bb
Jack <dongtianzheng@gmail.com>
2016-01-07 01:21:10 -0500

    parser

--- parser.ml
+++ parser.ml
@@ -5,7 +5,7 @@ type regex =
   | Char of char
   | Concatenation of regex * regex
   | Alternation of regex * regex
-  | Epsilon
+  (*| Epsilon*)
 
 
 exception IllegalExpression of string
@@ -33,7 +33,7 @@ let rec regex_to_string r : string =
   | Char c -> Char.escaped c
   | Concatenation (r1, r2) -> (regex_to_string r1) ^ (regex_to_string r2)
   | Alternation (r1, r2) -> (regex_to_string r1) ^ "|" ^ (regex_to_string r2)
-  | Epsilon -> ""
+  (*| Epsilon -> ""*)
 
 let string_to_char_list (str:string) : char list = 
   let rec helper (i:int) (col:char list) = 
@@ -63,17 +63,27 @@ let lookahead token_list =
 
 
 (**
- *  S = T X
- *  X = "|"S | E
- *  T = F Y
- *  Y = T F | E
- *  F = U Z
- *  Z = *F | E
- *  U = (S) | a .. z
+ * exp     = concat "|" exp | concat
+ * concat  = term concat | term
+ * term    = element* | element
+ * element = (exp) | a .. z 
  *
- *  First_Set(S) = { "|" }
- *  First_Set(T) = { "(", "a .. z"}
- *  First_Set(F) = { "*", "(", "a .. z"}
+ * exp = concat factor1
+ * factor1 = "|" exp | e
+ * concat = term factor2
+ * factor2 = concat | e
+ * term = element factor3
+ * factor3 = * | e
+ * element = (exp) | a .. z
+ *
+ * S = T X
+ * X = "|" S | E
+ * T = F Y 
+ * Y = T | E
+ * F = U Z
+ * Z = * | E
+ * U = (S) | a .. z
+ *
  **)
 
 let rec parse_S (l : token list) : (regex * token list) = 
@@ -87,24 +97,54 @@ let rec parse_S (l : token list) : (regex * token list) =
 
 and parse_T (l : token list) : (regex * token list) = 
   let (a1, l1) = parse_F l in
-  let (t, rest) = lookahead l1 in
+  let (t, rest) = lookahead l1 in 
   match t with
-  |
+  | Alphabet c -> (Char c, rest)
+  | LParen -> 
+     (let (a, l1) = parse_S rest in
+      let (t1, l2) = lookahead l1 in
+      match t1 with
+      | RParen -> (a, l2)
+      | _ -> raise (IllegalExpression "Unbalanced parentheses"))
+  | _ -> (a1, l1)
+
+
+and parse_F (l : token list) : (regex * token list) = 
+  let (a1, l1) = parse_U l in 
+  let (t, rest) = lookahead l1 in 
+  match t with
+  | Star -> (Closure a1, rest)
+  | _ -> (a1, l1)
+
+and parse_U l = 
+  let (t, rest) = lookahead l in
+  match t with
+  | Alphabet c -> (Char c, rest)
+  | LParen -> 
+     (let (a, l1) = parse_S rest in
+      let (t1, l2) = lookahead l1 in
+      match t1 with
+      | RParen -> (a, l2)
+      | _ -> raise (IllegalExpression "Unbalanced parentheses"))
+  | _ -> raise (IllegalExpression "Unknown token")
 
 
 
-let parse str = 
+let parse str : regex = 
   let tok_list = tokenize str in
   print_string "Input token list = " ;
   List.iter (fun c -> print_string (" " ^ (token_to_string c))) tok_list;
   print_endline "" ;
 
-  let (a, t) = parse_P1 tok_list in
+  let (a, t) = parse_S tok_list in
   List.iter (fun c -> print_string (" " ^ (token_to_string c))) t;
   print_endline "";
-  if t <> [End] then raise (IllegalExpression "last token is not END");
+  if t <> [End] then raise (IllegalExpression "last token is not END") else 
   a
 
 
 let _ = 
-  parse_P1 [Alphabet 'a'; LParen; Alphabet 'b'; Pipe; Alphabet 'c'; RParen; Star; End]
+  parse "a(b|c)*"
+  (*let (a, l) = parse_S [Alphabet 'a'; LParen; Alphabet 'b'; Pipe; Alphabet 'c'; RParen; Star; End] in *)
+  (*l = [End] *)
+  
