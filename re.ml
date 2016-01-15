c28fcf90fb148d5fa943311893e93672b1fe9adc
jack <dongtianzheng@gmail.com>
2016-01-15 14:13:05 -0500

    subset constr

--- re.ml
+++ re.ml
@@ -56,6 +56,8 @@ let step (n : nfa) (ss : int list) (c : char) : int list =
   in
   digin n.transactions reachables
 
+
+
 let match_re (re : string) (str : string) : bool = 
   let tokens = Ast.string_to_char_list str in
   let {
@@ -87,4 +89,39 @@ let match_re (re : string) (str : string) : bool =
   (*let new_ss = step n (reachables_from_start n n.start_state) 'a' in*)
   (*List.iter (fun x -> print_string ((string_of_int x) ^ " ")) new_ss *)
    
+
+let states_move (s : state list) (a : alphabet option) (ts : transaction list) : state list = 
+  failwith ""
+
+let move (s : state) (a : alphaber) (ts : transaction list) : state = 
+  failwith ""
+
+let follow_epsilon (s : state) (ts : transaction list) : state list = 
+  state_move [s] None ts
+
+let states_follow_epsilon (s : state list) (ts : transaction list) : state list = 
+  state_move s None ts
+
+
+let helper (n : nfa) (work_list : state list list) (d_states : state list list) 
+           (d : dict) (ts : transaction list) : d_states * d * t = 
+  match work_list with
+  | [] -> (d_states, d, t)
+  | hd :: tl ->
+    List.map
+      (fun c -> 
+        let t = states_follow_epsilon (states_move hd c ts) ts in
+        if (List.mem t d_state)
+        then (d_states, d, t)
+        else
+          helper n (t :: tl) (t :: d_states) ((Nfa.gen_state_num (), t) :: d) ts
+
+
+
+
+
+
+let nfa_to_dfa (n : nfa) : dfa = 
+  let d0_states = state_follow_epsilon n.start_state n_ts in
+  helper n d0_state [d0_states] ((Nfa.gen_state_num(), do_states) :: empty_dict) nfa.transactions
 
